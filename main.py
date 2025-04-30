10f9a99b2472d0ac98de1560ca27b9c7e06c1cb3
ElOsasInc <rosas.axel.b@gmail.com>
2025-04-30 04:18:01 -0600

    Cambio en login xd

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ DATABASE_URL = os.environ['DATABASE_URL']
 
 upiicsara.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
